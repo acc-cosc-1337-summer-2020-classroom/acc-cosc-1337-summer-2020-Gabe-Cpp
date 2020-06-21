3f293312982aaf1a973a5a756a2d6684b1a5cfed
Gabe-Cpp <gabriel.pintilie@g.austincc.edu>
2020-06-21 16:59:05 -0500

    adding 04_module 08_strings in examples

---
+++ src/examples/04_module/08_strings/strings.cpp
@@ -0,0 +1,42 @@
+#include "strings.h"
+#include<iostream>
+#include<string>
+
+using std::vector;
+
+/*
+Function loop_vector_w_for_ranged
+The function creates an int vector with values 9, 10, 99, 5, 67
+and displays the numbers to screen using a for ranged loop with auto as follows: 9 10 99 5  67
+
+@param none
+*/
+
+
+/*
+Function loop_vector_w_for_ranged_val
+
+Displays the numbers to screen using.
+
+@param vector<int>
+*/
+
+
+
+/*
+Function loop_vector_w_for_ranged_ref
+
+Displays the numbers to screen 
+
+@param vector<int>&
+*/
+
+
+/*
+Function loop_vector_w_for_ranged_const_ref
+
+Displays the numbers to screen
+
+@param const vector<int>&
+*/
+
