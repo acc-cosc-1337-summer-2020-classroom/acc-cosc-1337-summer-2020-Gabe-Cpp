8392c8e5ab81cc3b2d2d96f35f22aac5f8354932
Gabe-Cpp <gabriel.pintilie@g.austincc.edu>
2020-06-25 12:49:28 -0500

    a bit more work left on the HW3 main

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+#include <cctype>
+#include <string>
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +9,33 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
-
-
+double get_gc_content(const std::string & dna){
+    double gc_total = 0, gc_content=0;
+    int length= dna.length();
+        for(int i= 0; i < length; i++){
+        char ltr= dna[i];
+        if ((ltr == 'G') || (ltr == 'C')){
+            gc_total ++;
+        }
+    }
+    gc_content= gc_total/length;
+    return gc_content;
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+std::string get_reverse_string(std::string dna){
+    std::string reverse_string;
+    int length= dna.length();
+        for(int i= length-1; i >= 0; --i){
+            char ltr= dna[i];
+            reverse_string.push_back(ltr);
+        }
+    return reverse_string;
 
+}
 
 
 /*
@@ -28,3 +49,25 @@ c. return string
 
 */
 
+std::string get_dna_complement(std::string dna){
+    std::string complement_dna;
+    complement_dna= get_reverse_string (dna);
+    std::string complement_string;
+    int length= complement_dna.length();
+        for(int i= 0; i < length; i++){
+        char ltr= complement_dna[i];
+            if (ltr == 'A') {
+                complement_string.push_back('T');
+            }
+            if (ltr == 'T') {
+                complement_string.push_back('A');
+            }
+            if (ltr == 'C') {
+                complement_string.push_back('G');
+            }
+            else if (ltr == 'G') {
+                complement_string.push_back('C');
+            }
+        }
+    return complement_string;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
-
+#include<iostream>
+#include<string>
+#include<cctype>
+#include "dna.h"
 //write using statements
-
+using std::cout; using std::cin; using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,25 @@ user enters a y or Y.
 */
 int main() 
 {
+	char option;
+	do
+		{
+		string dna_input;
+		cout << "Enter DNA string: ";
+		do 			
+			{
+			cin >> dna_input;
+			int length= dna_input.length();
+        		for(int i= 0; i < length; i++){
+        			char ltr= dna_input[i];
+				}
+			cout << "Wrong letter(s)! Try again. " << '\n';
+			} while ((ltr != 'A') || (ltr != 'T') || (ltr != 'C')|| (ltr != 'G'));
+		cout << "DNA string: " << dna_input << '\n';
+
+		cout << "Another entry? (Y/N) ";
+		cin >> option;		
+		} while ((option == 'Y') || (option == 'y'));
+	
 	return 0;
 }
