e46134a34870bcc23a426e1f3d32186ce6f2a942
Gabe-Cpp <gabriel.pintilie@g.austincc.edu>
2020-06-25 16:19:51 -0500

    hw3_working code

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -13,27 +13,33 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
-int main() 
-{
+int main() {
 	char option;
-	do
-		{
-		string dna_input;
-		cout << "Enter DNA string: ";
-		do 			
-			{
-			cin >> dna_input;
-			int length= dna_input.length();
-        		for(int i= 0; i < length; i++){
-        			char ltr= dna_input[i];
-				}
-			cout << "Wrong letter(s)! Try again. " << '\n';
-			} while ((ltr != 'A') || (ltr != 'T') || (ltr != 'C')|| (ltr != 'G'));
+	string dna_input;
+	double gc_content;
+	string dna_complement;	
+    do{
+		cout << "Enter DNA string: "<< '\n';
+		cin >> dna_input;
 		cout << "DNA string: " << dna_input << '\n';
+		char test;
+		cout << "MENU"<< '\n';
+		cout << "Enter 1: Get GC Content "<< '\n';
+		cout << "Enter 2: Get DNA Complement "<< '\n';
+		cin >> test;
+		switch (test){
+			case '1':
+				gc_content= get_gc_content(dna_input);
+				cout << "GC Content= " << gc_content << '\n';
+				break;
+			case '2':
+				dna_complement= get_dna_complement(dna_input);
+				cout << "DNA Complement= " << dna_complement << '\n';
+		}
 
 		cout << "Another entry? (Y/N) ";
 		cin >> option;		
-		} while ((option == 'Y') || (option == 'y'));
+	} while ((option == 'Y') || (option == 'y'));
 	
 	return 0;
 }
